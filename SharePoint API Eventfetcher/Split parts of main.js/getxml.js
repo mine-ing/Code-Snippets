e568283d0b14f0ff82e7582ca5a70d2a92239bea
mine ing <101294355+mine-ing@users.noreply.github.com>
2022-03-10 13:43:27 +0100

    corrected placeholders in example url

--- SharePoint API Eventfetcher/Split parts of main.js/getxml.js
+++ SharePoint API Eventfetcher/Split parts of main.js/getxml.js
@@ -1,6 +1,6 @@
 const https = require('https');
 const fs = require('fs');
-https.get('https://USERNAME:PASSWORD@sharepoint.tuhh.de/sites/fks/_api/web/lists/getbytitle(\'CALENDAR_NAME\')/items', (response) => {
+https.get('https://USERNAME:PASSWORD@sharepoint.URL.TO/SITE/_api/web/lists/getbytitle(\'CALENDAR_NAME\')/items', (response) => {
     let data = '';
     response.on('data', (chunk) =>{
         data += chunk;
